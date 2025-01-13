2f26945d8b8dfb68e4f63c929410dd9f4dae537c
Angel <apermae@alu.upo.es>
2025-01-13 11:57:28 +0100

    a

--- EXAMENES/JS/Examen23-24/impJS.js
+++ EXAMENES/JS/Examen23-24/impJS.js
@@ -35,7 +35,24 @@ function generarUnidades(DEPTOS, SEDES, NUMUBICACIONES){
 }
 
 function validarFormulario(){
+    let valido=true;
+    let prioridadValido = validarLista(document.getElementById("priotity"));
+    let ubicacionValido = validarLista(document.getElementById("loc"));
+    let tipoIncidenciaValido = validarTipoIncidencia();
+    let caracteresValido = limitarCaracteres(document.getElementById("descripcion"), LIMITE_DE_CARACTERES);
+    let fechaValido = comprobarFormatoFecha(document.getElementById("date"));
 
+    if(prioridadValido===false || ubicacionValido===false || tipoIncidenciaValido===false || caracteresValido===false || fechaValido===false){
+        valido=false;
+        alert(ERRORES_EN_FORMULARIO);
+    }else{
+        let confirmar = confirm(AVISO_USUARIO);
+        if(confirmar===false){
+            valido=false;
+        }
+    }
+
+    return valido;
 }
 
 function validarTipoIncidencia(){
@@ -49,7 +66,7 @@ function validarTipoIncidencia(){
         }
     });
 
-    if(valid === false){
+    if(valido === false){
         mostrarErrores("incidentType", ERROR_TIPO_DE_INCIDENCIA_NO_SELECCIONADA);
     }
 
@@ -81,21 +98,70 @@ function mostrarErrores(campo, error){
 }
 
 function limitarCaracteres(campo, limite){
-
+    let spanError = eliminarErrores(campo);
+    if(campo.value.length<=0 || campo.value.length>limite){
+        mostrarErrores(campo.name, ERROR_SUPERADO_LIMITE_CARACTERES.replace("X", limite)+", has escrito "+campo.value.length+" caracteres.");
+        return false;
+    }else{
+        return true;
+    }
 }
 
 function comprobarFormatoFecha(campo){
-
+    let spanError = eliminarErrores(campo);
+    if(/^\d{2}\/\d{2}\/\d{4}$/.test(campo.value)===false){
+        mostrarErrores(campo.name, ERROR_FORMATO_FECHA);
+        return false;
+    }else{
+        return true;
+    }
 }
 
 function agregarFichero(){
+    let archivosContainer = document.getElementById("archivosContainer");
+    let files = document.querySelectorAll("input[type='file']");
 
+    files.forEach(file=>{
+        if(file.value===""){
+            return;
+        }
+    });
+
+    let nuevoCampo=document.createElement("input");
+    nuevoCampo.type="file";
+    nuevoCampo.id="file"+(files.length+1);
+    nuevoCampo.name="file"+(files.length+1);
+    nuevoCampo.onchange=comprobarAdjuntos;
+
+    archivosContainer.append(nuevoCampo);
+
+    btnAgregar.disabled=true;
+    document.getElementById("btnEliminar").disabled=false;
 }
 
 function eliminarFichero(){
+    let btnEliminar = document.getElementById("btnEliminar");
+    let files = document.querySelectorAll("input[type='file']");
 
+    if(files.length===1){
+        btnEliminar.disabled = true;
+    }else{
+        files[files.length-1].remove();
+        document.getElementById("btnAgregar").disabled=false;
+    }
 }
 
-function comporbarAdjuntos(){
+function comprobarAdjuntos(){
+    let files = document.querySelectorAll("input[type='file']");
 
+    files.forEach(file=>{
+        if(file.value===""){
+            document.getElementById("btnAgregar").disabled=true;
+            document.getElementById("btnEliminar").disabled=false;
+            return;
+        }
+    });
+
+    document.getElementById("btnAgregar").disabled=false;
+    document.getElementById("btnEliminar").disabled=false;
 }
